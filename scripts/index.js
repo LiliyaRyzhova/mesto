4966a07973e8327799ff2a094a1c0828a81a2b5f
Liliya Ryzhova <liliyyyyy2007@mail.ru>
2020-11-05 20:32:14 +0300

    project mesto v4.2

--- scripts/index.js
+++ scripts/index.js
@@ -1,33 +1,33 @@
 let popup = document.querySelector('.popup');
-let form = document.querySelector ('.popup__form');
+let editform = document.querySelector('.popup__form');
 let editButton = document.querySelector('.profile__edit-button');
 let popupCloseButton = document.querySelector('.popup__close-button');
 let nameProfile = document.querySelector('.profile__user-name');
 let professionProfile = document.querySelector('.profile__profession');
 let nameEditField = document.querySelector('.popup__field_name');
 let professionEditField = document.querySelector('.popup__field_profession');
-let saveButton= document.querySelector('.popup__save-button');
+let saveButton = document.querySelector('.popup__save-button');
 
 
-function showPopup () {
+function showPopup() {
   popup.classList.add('popup_opened');
-  nameEditField.value=nameProfile.textContent;
-  professionEditField.value=professionProfile.textContent;
+  nameEditField.value = nameProfile.textContent;
+  professionEditField.value = professionProfile.textContent;
 }
 
 
-function closePopup () {
-	 popup.classList.remove ('popup_opened');
+function closePopup() {
+  popup.classList.remove('popup_opened');
 }
 
 
-function submitPopup (event) {
-	  event.preventDefault();
-	  nameProfile.textContent=nameEditField.value;
-  	professionProfile.textContent=professionEditField.value;
-   closePopup ();
+function submitPopup(event) {
+  event.preventDefault();
+  nameProfile.textContent = nameEditField.value;
+  professionProfile.textContent = professionEditField.value;
+  closePopup();
 }
 
 editButton.addEventListener('click', showPopup);
 popupCloseButton.addEventListener('click', closePopup);
-form.addEventListener('submit', submitPopup);
+editform.addEventListener('submit', submitPopup);
