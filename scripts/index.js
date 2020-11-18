8b312de2a3274458a15f38ac137988f2f17ffe36
Liliya Ryzhova <liliyyyyy2007@mail.ru>
2020-11-19 00:00:52 +0300

    project mesto W5 V1

--- scripts/index.js
+++ scripts/index.js
@@ -1,33 +1,135 @@
-let popup = document.querySelector('.popup');
-let editForm = document.querySelector('.popup__form');
-let editButton = document.querySelector('.profile__edit-button');
-let popupCloseButton = document.querySelector('.popup__close-button');
-let nameProfile = document.querySelector('.profile__user-name');
-let professionProfile = document.querySelector('.profile__profession');
-let nameEditField = document.querySelector('.popup__field_name');
-let professionEditField = document.querySelector('.popup__field_profession');
-let saveButton = document.querySelector('.popup__save-button');
+const popup = document.querySelector('.popup');
+const editForm = document.querySelector('.popup__form_edit-profile');
+const addForm = document.querySelector('.popup__form_add-card')
+const showImagePopup = document.querySelector('.popup_open-image');
+const editButton = document.querySelector('.profile__edit-button');
+const popupCloseButtonEditPopup = document.querySelector('.popup__close-button_edit-popup');
+const popupCloseButtonAddPopup = document.querySelector('.popup__close-button_add-popup');
+const popupCloseButtonShowImagePopup = document.querySelector('.popup__close-button_open-image');
+const nameProfile = document.querySelector('.profile__user-name');
+const professionProfile = document.querySelector('.profile__profession');
+const nameEditField = document.querySelector('.popup__field_name');
+const professionEditField = document.querySelector('.popup__field_profession');
+const saveButton = document.querySelector('.popup__save-button');
+const cardsContainer = document.querySelector('.cards');
+const addCardButton = document.querySelector('.profile__add-button');
+const popupEditProfile = document.querySelector('.popup_edit-profile');
+const popupAddCard = document.querySelector('.popup_add-card');
+const placeField = document.querySelector('.popup__field_place-name');
+const linkField = document.querySelector('.popup__field_link');
+const likeButton = document.querySelector('.card__like-button');
+const cardName = document.querySelector('.card__place-name');
+const popupShowImageCaption = document.querySelector('.popup__image-caption');
+const cardImage = document.querySelector('.popup__image');
 
 
-function showPopup() {
+
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+function showPopup(popup) {
   popup.classList.add('popup_opened');
-  nameEditField.value = nameProfile.textContent;
-  professionEditField.value = professionProfile.textContent;
 }
 
-
-function closePopup() {
+function closePopup(popup) {
   popup.classList.remove('popup_opened');
 }
 
+// редактирование профиля//
 
-function submitPopup(event) {
+function fillProfilePopup() {
+  nameEditField.value = nameProfile.textContent;
+  professionEditField.value = professionProfile.textContent;
+  showPopup(popupEditProfile);
+
+}
+
+function submitPopupEditProfile(event) {
   event.preventDefault();
   nameProfile.textContent = nameEditField.value;
   professionProfile.textContent = professionEditField.value;
-  closePopup();
+  closePopup(popupEditProfile);
 }
 
-editButton.addEventListener('click', showPopup);
-popupCloseButton.addEventListener('click', closePopup);
-editForm.addEventListener('submit', submitPopup);
+// создание, редактирование карточек//
+
+function createCard(data) {
+  const cardTemplate = document.querySelector('#card-template').content;
+  const cardElement = cardTemplate.cloneNode(true);
+  const cardImage = cardElement.querySelector('.card__image');
+  const cardCaption = cardElement.querySelector('.card__place-name');
+  const removeButton = cardElement.querySelector('.card__remove-button');
+  cardImage.src = data.link;
+  cardImage.alt = data.name;
+  cardCaption.textContent = data.name;
+  cardElement.querySelector('.card__image').src = data.link;
+  cardElement.querySelector('.card__place-name').textContent = data.name;
+  cardElement.querySelector('.card__like-button').addEventListener('click', function (event) {
+    event.target.classList.toggle('card__like-button_active')});
+  removeButton.addEventListener('click', function () {
+    const cardItem = removeButton.closest('.card');
+    cardItem.remove();
+  })
+  cardImage.addEventListener('click', () => showImage(data.link, data.name));
+  return cardElement;
+}
+
+function addInitialCards (data) {
+  const cardElement = createCard(data);
+  cardsContainer.append(cardElement);
+}
+
+initialCards.forEach(addInitialCards);
+
+function addCard(event) {
+  event.preventDefault();
+  const data = {
+    link: linkField.value,
+    name: placeField.value
+  };
+  const cardElement = createCard(data);
+  cardsContainer.prepend(cardElement);
+  closePopup(popupAddCard);
+}
+
+function showImage (src, alt) {
+  cardImage.src = src;
+  cardImage.alt = alt;
+  popupShowImageCaption.textContent = alt;
+  showPopup(showImagePopup);
+}
+
+
+
+editButton.addEventListener('click', () => fillProfilePopup(popupEditProfile));
+addCardButton.addEventListener('click', () => showPopup(popupAddCard));
+popupCloseButtonEditPopup.addEventListener('click', () => closePopup(popupEditProfile));
+popupCloseButtonAddPopup.addEventListener('click', () => closePopup(popupAddCard));
+popupCloseButtonShowImagePopup.addEventListener('click', () => closePopup(showImagePopup));
+editForm.addEventListener('submit', submitPopupEditProfile);
+addForm.addEventListener('submit', addCard);
+
